bf8832d5af0c16bca044024506463464833a7fc5
Kavya <kavya.murali@techdome.net.in>
2022-02-01 01:57:49 +0530

    Updates

---
+++ precache-manifest.4e86cee22b98760ccb30ff9093f5d93c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d73272c65d4231d72ced6274c8c49397",
+    "url": "/gallery-gram/index.html"
+  },
+  {
+    "revision": "0ec00a1d8bc98bd9d8a6",
+    "url": "/gallery-gram/static/css/main.48de7a29.chunk.css"
+  },
+  {
+    "revision": "d4686c4032e54543f834",
+    "url": "/gallery-gram/static/js/2.779c2cce.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/gallery-gram/static/js/2.779c2cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec00a1d8bc98bd9d8a6",
+    "url": "/gallery-gram/static/js/main.486245bc.chunk.js"
+  },
+  {
+    "revision": "8555b482b3f0722d770e",
+    "url": "/gallery-gram/static/js/runtime-main.abbbfd5e.js"
+  }
+]);
